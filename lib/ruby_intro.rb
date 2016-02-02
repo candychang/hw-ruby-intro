86b02de766edf4a7b4399e07c0cbd315ac2fd737
Candy Chang <candyichang@gmail.com>
2016-02-02 07:46:31 +0000

    finished parts 1 and 2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,29 +3,49 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  curr = 0
+  if arr.empty?
+    curr
+  else
+    arr.each { |a| curr += a }
+    curr
+  end
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.empty?
+    0
+  elsif arr.size == 1
+    arr[0]
+  else
+    max = arr.max(2)
+    sum(max)
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.empty?
+    false
+  else
+    newarr = arr.combination(2).map { |a| sum(a) }
+    newarr.include?(n)
+  end
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s.match(/^[b-df-hj-np-tv-z]/i)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s.match(/^(0|1)+$/)
+    s.match(/^0$/) or s.match(/(0{2,})$/)
+  end
 end
 
 # Part 3
